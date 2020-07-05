f1d132d50d630a9a9eeba604d7da6fc4576c13b3
edoardottt <edoardott@gmail.com>
2020-07-05 18:24:01 +0200

    update script design

--- add_rows_by_file.py
+++ add_rows_by_file.py
@@ -5,38 +5,151 @@
 https://edoardoottavianelli.it
 """
 
-import sys
+import csv
 
-def add_func():
-    word = "scheduled.txt"
-    new_file = 'README.md'
-    with open(word) as f:
-        text = f.read().split()
-    with open(new_file) as f:
-        t = f.read()
-    ls = []
-    for elem in text:
-        if elem in t: ls.append(elem)
-    if len(ls)!=0: 
-        print("Already inside: " + str(len(ls)))
-        for elem in ls:
-            text.remove(elem)
-    count = 0
-    with open(new_file,'a+') as f:
-        for i in range(len(text)):
-            count += 1
-            elem = text[i]
+def check_duplicate_readme():
+    print("[-] Checking duplicates in README...")
+    with open("creators.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        creators = []
+        for row in csv_reader:
+            if line_count == 0: line_count += 1
+            else:
+                creators.append(row[0])
+    with open("README.md") as f:
+        text = f.read()
+    duplicates = []
+    for creator in creators:
+        creator = " " + format_user(creator) + " "
+        if text.count(creator) > 1 and creator not in duplicates:
+            duplicates.append(creator)
+    return duplicates
+
+def check_duplicate_creators():
+    print("[-] Checking duplicates in creators...")
+    with open("creators.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        creators = []
+        for row in csv_reader:
+            if line_count == 0: line_count += 1
+            else:
+                creators.append(row[0])
+    duplicates = []
+    for creator in creators:
+        if creators.count(creator) > 1 and creator not in duplicates:
+            duplicates.append(creator)
+    return duplicates
+
+def check_duplicate_scheduled():
+    print("[-] Checking duplicates in scheduled...")
+    creators = read_scheduled()
+    duplicates = []
+    for creator in creators:
+        if creators.count(creator) > 1 and creator not in duplicates:
+            duplicates.append(creator)
+    return duplicates
+
+def insert_users_readme(users,not_ok):
+    for elem in not_ok: users.remove(elem)
+    print("[-] Inserting users in README...")
+    with open("README.md","a+") as f:
+        for i in range(len(users)):
+            elem = users[i]
             stri = stringed(elem)
             f.write(stri)
-    print("Added {} creators".format(count))
-    with open(word,"w") as f:
-        f.write("placeholder")
-    return 'OK'
+    print("[+] Added {} creators into README.".format(len(users)))
 
+def insert_users_creators(users,not_ok):
+    for elem in not_ok: users.remove(elem)
+    print("[-] Inserting users in creators...")
+    with open("creators.csv", mode='a') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for elem in users:
+            elem = elem + ","
+            writer.writerow([elem])
+    print("[+] Added {} creators into creators.".format(len(users)))
+
+def present_in_readme(users):
+    print("[-] Checking if users already in README...")
+    with open("README.md") as f:
+        text = f.read()
+    present = []
+    for elem in users:
+        if " " + format_user(elem) + " " in text: present.append(elem)
+    return present
+
+def present_in_creators(users):
+    print("[-] Checking if users already in creators...")
+    with open("creators.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        creators = []
+        for row in csv_reader:
+            if line_count == 0: line_count += 1
+            else:
+                creators.append(row[0])
+    present = []
+    for user in users:
+        if user in creators:
+            present.append(user)
+    return present    
+
+def read_scheduled():
+    with open("scheduled.txt") as f:
+        text = f.read().split()
+    return text
+
+def format_user(elem):
+    result = elem.replace("_","\_")    
+    return result
+    
 def stringed(elem):
-	if elem[-1]!='_': stri = '| '+elem+' | '+'https://instagram.com/'+elem+' |\n'
-	else:
-		stri = '| '+elem+' | '+'['+'https://instagram.com/'+elem+'](https://instagram.com/'+elem+') |\n'
-	return stri
+    if "_" not in elem: stri = '| '+elem+' | '+'https://instagram.com/'+elem+' |\n'
+    else:
+        elem_ok = format_user(elem)
+        stri = '| '+elem_ok+' | '+'['+'https://instagram.com/'+elem_ok+'](https://instagram.com/'+elem+') |\n'
+    return stri
 
-print(add_func())
+def flush_scheduled():
+    with open("scheduled.txt","w") as f:
+        f.write("placeholder")
+    print("[+] Scheduled flushed!")
+
+def add_func():
+    candidates = read_scheduled()
+    if candidates[0] == "placeholder":
+        print("[!] Scheduled empty.")
+        return 0
+    duplicates = check_duplicate_readme()
+    if len(duplicates) > 0:
+        print("[!] Duplicates found in README!")
+        print(duplicates)
+        return 0
+    duplicates = check_duplicate_creators()
+    if len(duplicates) > 0:
+        print("[!] Duplicates found in creators!")
+        print(duplicates)
+        return 0
+    duplicates = check_duplicate_scheduled()
+    if len(duplicates) > 0:
+        print("[!] Duplicates found in scheduled!")
+        print(duplicates)
+        return 0
+    candidates = read_scheduled()
+    not_ok = present_in_readme(candidates)
+    if len(not_ok) > 0:
+        print("[!] Users already in README!")
+        print(not_ok)
+    insert_users_readme(candidates,not_ok)
+    not_ok = present_in_creators(candidates)
+    if len(not_ok) > 0:
+        print("[!] Users already in creators!")
+        print(not_ok)
+    insert_users_creators(candidates,not_ok)
+    flush_scheduled()
+    print("[#] Finished!")
+
+if __name__ == "__main__":
+    add_func()

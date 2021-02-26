c3159feaf7575ceaa6b2238690c9be5a61c41998
edoardottt <edoardott@gmail.com>
2021-02-26 21:07:16 +0100

    update

---
+++ download.js
@@ -0,0 +1,10 @@
+var array = [];
+var links = document.getElementsByTagName("a");
+for(var i=0; i<links.length; i++) {
+    var sub = links[i].href.substring(23,27);
+    if (sub == "user") {
+    	var user = links[i].href.substring(28);
+    	array.push(user);
+    }
+}
+JSON.stringify(array)

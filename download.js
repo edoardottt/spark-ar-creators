89f66ae1240030ea7022b95ee739cb259fbc46bd
edoardottt <edoardott@gmail.com>
2021-07-03 10:26:28 +0200

    update

--- download.js
+++ download.js
@@ -1,11 +1,10 @@
 // paste this on web console
 var array = [];
 var links = document.getElementsByTagName("a");
-for(var i=0; i<links.length; i++) {
-    var sub = links[i].href.substring(23,27);
-    if (sub == "user") {
-    	var user = links[i].href.substring(28);
-    	array.push(user);
+for (var i = 0; i < links.length; i++) {
+    var sub = links[i].href;
+    if (sub.substring(0, 22) == "https://instagram.com/") {
+        array.push(sub);
     }
 }
 JSON.stringify(array)

7ad0023f969bf400b634f7d826d2eadcb41ddd3a
edoardottt <edoardott@gmail.com>
2021-09-16 10:11:23 +0200

    update

--- download.js
+++ download.js
@@ -3,7 +3,7 @@ var array = [];
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
     var sub = links[i].href;
-    if (sub.substring(0, 22) == "https://instagram.com/") {
+    if (sub.substring(0, 22) == "https://instagram.com/" || sub.substring(0,25) == "https://www.instagram.com/") {
         array.push(sub);
     }
 }

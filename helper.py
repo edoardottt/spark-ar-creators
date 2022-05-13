1df1210128876f9d5e8d4f961adc417eacb5aa50
edoardottt <edoardott@gmail.com>
2022-05-13 18:08:35 +0200

    beautified output

--- helper.py
+++ helper.py
@@ -263,17 +263,6 @@ def flush_scheduled():
 
 
 def print_banner():
-    """
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-    """
     print(bcolors.HEADER + "========================================" + bcolors.ENDC)
     print(bcolors.HEADER + "            SPARK AR CREATORS           " + bcolors.ENDC)
     print(bcolors.HEADER + "========================================" + bcolors.ENDC)

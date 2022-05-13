eeeca10a76fd532eedfa0e65a7672d41926f7895
edoardottt <edoardott@gmail.com>
2022-05-13 18:08:21 +0200

    beautified output

--- helper.py
+++ helper.py
@@ -263,14 +263,30 @@ def flush_scheduled():
 
 
 def print_banner():
-    print(bcolors.HEADER + "==================================" + bcolors.ENDC)
-    print(bcolors.HEADER + "         SPARK AR CREATORS        " + bcolors.ENDC)
-    print(bcolors.HEADER + "==================================" + bcolors.ENDC)
+    """
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    """
+    print(bcolors.HEADER + "========================================" + bcolors.ENDC)
+    print(bcolors.HEADER + "            SPARK AR CREATORS           " + bcolors.ENDC)
+    print(bcolors.HEADER + "========================================" + bcolors.ENDC)
     print("")
-    print("        > @author: edoardottt")
-    print("        > https://www.edoardoottavianelli.it")
-    print("        > https://github.com/edoardottt/spark-ar-creators")
-    print("        > https://instagram.com/edoardottt")
+    print(bcolors.BOLD + "    > @author: edoardottt" + bcolors.ENDC)
+    print("")
+    print(bcolors.OKGREEN + "    > https://www.edoardoottavianelli.it")
+    print(
+        bcolors.OKCYAN
+        + "    > https://github.com/edoardottt/spark-ar-creators"
+        + bcolors.ENDC
+    )
+    print(bcolors.OKBLUE + "    > https://instagram.com/edoardottt" + bcolors.ENDC)
     print("")
 
 
